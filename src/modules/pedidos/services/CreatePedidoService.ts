2c5a84a72c4fc0fa270f3e51b15e9befcf8ef690
VostokI <alexandrecostatoo@gmail.com>
2022-02-18 14:17:53 -0300

    ..

--- src/modules/pedidos/services/CreatePedidoService.ts
+++ src/modules/pedidos/services/CreatePedidoService.ts
@@ -15,23 +15,23 @@ export default class CreatePedidoService {
     const cliente = new FindClientByIdService();
     let soma = 0;
 
-    //não permite que o usuário insira o id do pedido
+    
     if (data.id) {
       throw new AppError("ID não deve ser enviado no cadastro");
     }
 
-    //um pedido deve ter pelo menos um produto
+    
     if (data.pedido_produtos.length === 0) {
       throw new AppError("O pedido deve ter pelo menos um produto informado");
     }
 
     for (var i = 0; i < data.pedido_produtos.length; i++) {
-      //deve ser informado a quantidade de cada produto ao fazer o pedido
+      
       if (data.pedido_produtos[i].quantidade <= 0) {
         throw new AppError("A quantidade do produto deve ser superior a zero");
       }
 
-      //um pedido não pode ser finalizado se algum dos produtos estiver sem o estoque necessário
+     
       let produtos = await produto.execute(data.pedido_produtos[i].produto_id);
 
       if (data.pedido_produtos[i].produto_id === produtos.id) {
@@ -40,7 +40,7 @@ export default class CreatePedidoService {
         }
       }
 
-      //o valor do pedido deve ser calculado pelo sistema
+      
       soma += data.pedido_produtos[i].quantidade * produtos.preco;
     }
 
